80b4fef4c656d4aa6794fa6ef9571097a298d5a9
Naveen <automationwithnaveen@gmail.com>
2025-07-20 09:58:02 +0100

    EMP SAL Validation

---
+++ TestingDemoExamples/src/com/sample/test/EmployeeSalaryValidation.java
@@ -0,0 +1,19 @@
+package com.sample.test;
+
+public class EmployeeSalaryValidation 
+{
+	float salary;
+
+	public float getSalary() {
+		return salary;
+	}
+
+	public void setSalary(float salary) {
+		this.salary = salary;
+	}
+
+	public EmployeeSalaryValidation(float salary) {
+		super();
+		this.salary = salary;
+	}
+}
